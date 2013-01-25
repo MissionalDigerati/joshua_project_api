a74f1914460044f54dfba6336df852578ae6efd1
Codemis <johnathan@missionaldigerati.com>
2013-01-25 09:12:27 -0800

    Added license details.

--- App/Includes/CommonFunctions.php
+++ App/Includes/CommonFunctions.php
@@ -1,5 +1,26 @@
 <?php
 /**
+ * This file is part of Joshua Project API.
+ * 
+ * Joshua Project API is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Joshua Project API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see 
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ * @copyright Copyright 2013 Missional Digerati
+ * 
+ */
+/**
  * Convert an array of data to XML
  *
  * @param array $data The data array to be converted to XML
---
+++ README.md
@@ -0,0 +1,11 @@
+Joshua Project API
+------------------
+
+An API for connecting to the [Joshua Project](http://www.joshuaproject.net) data.  It uses the [PHP Slim Framework](http://slimframework.com). Still under construction.
+
+Development
+-----------
+
+Questions or problems? Please post them on the [issue tracker](https://github.com/MissionalDigerati/vts_cakephp_plugin/issues). You can contribute changes by forking the project and submitting a pull request.
+
+This script is created by Johnathan Pulos and is under the [GNU General Public License v3](http://www.gnu.org/licenses/gpl-3.0-standalone.html).
