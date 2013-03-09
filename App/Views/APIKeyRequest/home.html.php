05864fff3428527aff6c1b914e80b1465d948cec
Codemis <johnathan@missionaldigerati.com>
2013-03-09 15:10:34 -0800

    Added twitter bootstrap design to index.

--- App/Views/APIKeyRequest/home.html.php
+++ App/Views/APIKeyRequest/home.html.php
@@ -1,2 +1,36 @@
-<h1>Welcome to the Joshua Project API</h1>
-<p>This is a new development of <a href="http://www.missionaldigerati.org">Missional Digerati</a>.</p>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/styles.css" rel="stylesheet" media="screen">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="brand" href="#">Joshua Project API</a>
+				<div class="nav-collapse collapse">
+					<ul class="nav pull-right">
+						<li class="active"><a href="#">Home</a></li>
+						<li><a href="http://www.joshuaproject.net/">Joshua Project</a></li>
+						<li><a href="http://www.missionaldigerati.org">Missional Digerati</a></li>
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</div>
+	</div>
+    <div class="container">
+    	<h2>Welcome to the Joshua Project API</h2>
+		<p>This is a new development of <a href="http://www.missionaldigerati.org">Missional Digerati</a>.</p>
+    </div>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
