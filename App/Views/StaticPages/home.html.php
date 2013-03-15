99f8cbe5eeecafb7c70d65e37e46e0f8416e37b7
Codemis <johnathan@missionaldigerati.com>
2013-03-15 15:32:37 -0500

    Added testing for APIKeys controller.

--- App/Views/StaticPages/home.html.php
+++ App/Views/StaticPages/home.html.php
@@ -3,11 +3,12 @@
   <head>
     <title>Joshua Project API</title>
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
     <link href="css/styles.css" rel="stylesheet" media="screen">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
-	<div class="navbar navbar-fixed-top">
+	<div class="navbar">
 		<div class="navbar-inner">
 			<div class="container">
 				<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
