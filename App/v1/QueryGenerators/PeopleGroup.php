5a819076b92d5cc80097dc7aaa6a78d9fad9b666
Codemis <johnathan@missionaldigerati.com>
2013-08-27 20:40:43 -0700

    Added ProgressScaleText.

--- App/v1/QueryGenerators/PeopleGroup.php
+++ App/v1/QueryGenerators/PeopleGroup.php
@@ -95,6 +95,13 @@ class PeopleGroup
      */
     private $countryURLSelect = "CONCAT('http://www.joshuaproject.net/countries.php?rog3=', ROG3)";
     /**
+     * The CASE statement for generating the ProgressScaleText
+     *
+     * @var string
+     * @access private
+     */
+    private $progressScaleTextSelect = "CASE  WHEN JPScale = 1.0 OR JPScale = 2.0 THEN 'Status Unavailable' WHEN JPScale = 1.1 OR JPScale = 1.2 THEN 'Unreached' WHEN JPScale = 2.1 OR JPScale = 2.2 THEN 'Nominal Church' ELSE 'Established Church' END";
+    /**
      * Construct the class
      *
      * @param array $getParams the params to use for the query.  Each message has required fields, and will throw error
@@ -110,6 +117,7 @@ class PeopleGroup
         $this->selectFieldsStatement .= ", " . $this->peopleGroupURLSelect . " as PeopleGroupURL";
         $this->selectFieldsStatement .= ", " . $this->peopleGroupPhotoURLSelect . " as PeopleGroupPhotoURL";
         $this->selectFieldsStatement .= ", " . $this->countryURLSelect . " as CountryURL";
+        $this->selectFieldsStatement .= ", " . $this->progressScaleTextSelect . " as JPScaleText";
         $this->cleanParams();
     }
     /**
--- Tests/v1/Unit/QueryGenerators/PeopleGroupTest.php
+++ Tests/v1/Unit/QueryGenerators/PeopleGroupTest.php
@@ -302,6 +302,46 @@ class PeopleGroupTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expectedURL, strtolower($data[0]['CountryURL']));
     }
     /**
+     * Should return the correct JPScaleText Equal to Unreached
+     *
+     * @return void
+     * @access public
+     * @author Johnathan Pulos
+     */
+    public function testQueryShouldReturnCorrectJPScaleTextEqualToUnreached()
+    {
+        $paramData = array('id' => '12662', 'country' => 'CB');
+        $expectedText = "unreached";
+        $peopleGroup = new \QueryGenerators\PeopleGroup($paramData);
+        $peopleGroup->findByIdAndCountry();
+        $statement = $this->db->prepare($peopleGroup->preparedStatement);
+        $statement->execute($peopleGroup->preparedVariables);
+        $data = $statement->fetchAll(\PDO::FETCH_ASSOC);
+        $this->assertGreaterThan(1, $data[0]['JPScale']);
+        $this->assertLessThan(2, $data[0]['JPScale']);
+        $this->assertEquals($expectedText, strtolower($data[0]['JPScaleText']));
+    }
+    /**
+     * Should return the correct JPScaleText Equal to Nominal Church
+     *
+     * @return void
+     * @access public
+     * @author Johnathan Pulos
+     */
+    public function testQueryShouldReturnCorrectJPScaleTextEqualToNominalChurch()
+    {
+        $paramData = array('id' => '16153', 'country' => 'IN');
+        $expectedText = "nominal church";
+        $peopleGroup = new \QueryGenerators\PeopleGroup($paramData);
+        $peopleGroup->findByIdAndCountry();
+        $statement = $this->db->prepare($peopleGroup->preparedStatement);
+        $statement->execute($peopleGroup->preparedVariables);
+        $data = $statement->fetchAll(\PDO::FETCH_ASSOC);
+        $this->assertGreaterThan(2, $data[0]['JPScale']);
+        $this->assertLessThan(3, $data[0]['JPScale']);
+        $this->assertEquals($expectedText, strtolower($data[0]['JPScaleText']));
+    }
+    /**
      * findByIdAndCountry() should require an ID
      *
      * @return void
