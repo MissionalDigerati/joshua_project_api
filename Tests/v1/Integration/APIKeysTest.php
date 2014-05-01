b9576a167e43760242bf244aae015007950c65e0
Codemis <johnathan@missionaldigerati.com>
2014-05-01 11:42:56 -0700

    Cleaned up the warnings.

--- Tests/v1/Integration/APIKeysTest.php
+++ Tests/v1/Integration/APIKeysTest.php
@@ -210,7 +210,9 @@ class APIKeysTest extends \PHPUnit_Framework_TestCase
             array('name' => 'should_set_authorize_token', 'email' => 'joe@gmail.com', 'usage' => 'testing'),
             "should_set_authorize_token"
         );
-        $statement = $this->db->query("SELECT authorize_token from `md_api_keys` WHERE `name` = 'should_set_authorize_token'");
+        $statement = $this->db->query(
+            "SELECT authorize_token from `md_api_keys` WHERE `name` = 'should_set_authorize_token'"
+        );
         $data = $statement->fetchAll(\PDO::FETCH_ASSOC);
         $this->assertNotNull($data[0]['authorize_token']);
         $this->assertNotEmpty($data[0]['authorize_token']);
@@ -231,7 +233,9 @@ class APIKeysTest extends \PHPUnit_Framework_TestCase
             array('name' => 'i_should_become_active', 'email' => 'joe@gmail.com', 'usage' => 'testing'),
             "i_should_become_active"
         );
-        $statement = $this->db->query("SELECT authorize_token from `md_api_keys` WHERE `name` = 'i_should_become_active'");
+        $statement = $this->db->query(
+            "SELECT authorize_token from `md_api_keys` WHERE `name` = 'i_should_become_active'"
+        );
         $data = $statement->fetchAll(\PDO::FETCH_ASSOC);
         $this->cachedRequest->get(
             "http://joshua.api.local/get_my_api_key",
