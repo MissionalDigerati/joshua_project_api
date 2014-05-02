848414c1e520cc22f80f21413125cfbe26a2a69e
Codemis <johnathan@missionaldigerati.com>
2014-05-01 21:52:30 -0700

    Testing out linter.

--- App/v1/Resources/StaticPages.php
+++ App/v1/Resources/StaticPages.php
@@ -16,7 +16,7 @@
  * along with this program.  If not, see 
  * <http://www.gnu.org/licenses/>.
  *
- * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org> 
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * 
  */
