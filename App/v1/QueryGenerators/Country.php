dcbbc094cffbaf770dce80b818df134ca2f95a7a
Codemis <johnathan@jpulos.com>
2015-02-17 12:35:54 -0800

    Removed several fields from the countries table.

--- App/v1/QueryGenerators/Country.php
+++ App/v1/QueryGenerators/Country.php
@@ -1,24 +1,24 @@
 <?php
 /**
  * This file is part of Joshua Project API.
- * 
+ *
  * Joshua Project API is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Joshua Project API is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see 
+ * along with this program.  If not, see
  * <http://www.gnu.org/licenses/>.
  *
  * @author Johnathan Pulos <johnathan@missionaldigerati.org>
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
- * 
+ *
  */
 namespace QueryGenerators;
 
@@ -63,10 +63,9 @@ class Country extends QueryGenerator
         'PercentUnknown', 'ROL3OfficialLanguage', 'ROL3SecondaryLanguage', 'RLG3Primary', 'RegionCode',
         'InternetCtryCode', 'ROG3', 'ISO3', 'ISO2', 'ROG2', 'RegionName', 'AltName', 'Capital', 'Population',
         'PopulationSource', 'PoplGrowthRate', 'AreaSquareMiles', 'AreaSquareKilometers', 'PopulationPerSquareMile',
-        'CountryPhoneCode', 'SecurityLevel', 'LibraryCongressReportExists', 'IsCountry', 'StonyGround',
-        'USAPostalSystem', 'ReligionDataYear', 'LiteracyCategory', 'LiteracyRate', 'LiteracyRange', 'LiteracySource',
-        'CountryNotes', 'PercentDoublyProfessing', 'HDIYear', 'HDIValue', 'HDIRank', 'StateDeptReligiousFreedom',
-        'Source', 'UNMap', 'PercentUrbanized', 'PrayercastVideo', 'WINCountryProfile');
+        'CountryPhoneCode', 'SecurityLevel', 'ReligionDataYear', 'LiteracyCategory', 'LiteracyRate', 'LiteracyRange',
+        'LiteracySource', 'CountryNotes', 'PercentDoublyProfessing', 'HDIYear', 'HDIValue', 'HDIRank',
+        'StateDeptReligiousFreedom', 'Source', 'UNMap', 'PercentUrbanized', 'PrayercastVideo', 'WINCountryProfile');
     /**
      * The Database table to pull the data from.
      *
