6ff2a3d67827729db4bbd21d0edbb6bfe0172f87
Codemis <johnathan@jpulos.com>
2024-08-29 10:54:41 -0700

    Added an all method to the query generator for totals.

---
+++ App/v1/QueryGenerators/Total.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * This file is part of Joshua Project API.
+ *
+ * Joshua Project API is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Joshua Project API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ */
+
+declare(strict_types=1);
+
+namespace QueryGenerators;
+
+/**
+ * Generates the PDO prepared statements and variables for Totals.
+ *
+ * A class that creates the prepared statement, and sets up the variables for a PDO prepared statement query.
+ * Once you call a method like findById,  you can get the prepared statement by reading the class variable
+ * $preparedStatement.  You can retrieve the prepared variables by reading the class variable $preparedVariables.
+ * So here is an example using the Continents Query Generator to find a continent by id:
+ * <pre><code>
+ * &lt;?php
+ * // Initialize the class, and pass in the id.
+ * $totals = new \QueryGenerators\Total([]);
+ * // Call the method you want.
+ * $totals->findById();
+ * // Using PDO prepare the statement.
+ * $statement = $db->prepare($totals->preparedStatement);
+ * // Execute the query with the prepared params.
+ * $statement->execute($totals->preparedVariables);
+ * // Fetch the final results.
+ * $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+ * ?&gt;
+ * </code></pre>
+ *
+ * @author Johnathan Pulos
+ * @package QueryGenerators
+ */
+class Total extends QueryGenerator
+{
+    /**
+     * The database table to pull the data from.
+     *
+     * @var     string
+     * @access  protected
+     */
+    protected $tableName = 'jptotals';
+    /**
+     * A string that will hold the default MySQL ORDER BY for the Select statement.
+     *
+     * @var     string
+     * @access  protected
+     */
+    protected $defaultOrderByStatement = 'ORDER BY ID ASC';
+    /**
+     * An array of column names for this database table that we want to select in searches.
+     * Simply remove fields you do not want to expose.
+     *
+     * @var     array
+     * @access  protected
+     */
+    protected $fieldsToSelectArray = ['ID AS id', 'IDValue AS Value', 'RoundPrecision'];
+    /**
+     * Construct the Totals class.
+     *
+     * During construction,  the $getParams are checked and inserted in the $providedParams class variable.
+     * Some of the methods in this class require certain keys to be set, or it will throw an error.  The comments will
+     * state the required keys.
+     *
+     * @param   array   $getParams  The GET params to use for the query.
+     * @return  void
+     * @access  public
+     */
+    public function __construct(array $getParams)
+    {
+        parent::__construct($getParams);
+        $this->selectFieldsStatement = join(', ', $this->fieldsToSelectArray);
+    }
+    
+    /**
+     * Find all the totals in the table
+     *
+     * @return void
+     * @access  public
+     */
+    public function all(): void
+    {
+        $this->preparedStatement = "SELECT {$this->selectFieldsStatement} FROM {$this->tableName} {$this->defaultOrderByStatement}";
+        $this->preparedVariables = [];
+    }
+}
---
+++ Tests/v1/Unit/QueryGenerators/TotalTest.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of Joshua Project API.
+ *
+ * Joshua Project API is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Joshua Project API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ */
+namespace Tests\v1\Unit\QueryGenerators;
+
+use PHPToolbox\PDODatabase\PDODatabaseConnect;
+use PHPUnit\Framework\TestCase;
+
+class TotalTest extends TestCase
+{
+    private $db;
+
+    public function setUp(): void
+    {
+        $this->db = getDatabaseInstance();
+    }
+
+    public function testAllShouldReturnAllTotals()
+    {
+        $totals = new \QueryGenerators\Total([]);
+        $totals->all();
+        $this->assertNotEmpty($totals->preparedStatement);
+        $this->assertEmpty($totals->preparedVariables);
+        $statement = $this->db->prepare($totals->preparedStatement);
+        $statement->execute($totals->preparedVariables);
+        $data = $statement->fetchAll(\PDO::FETCH_ASSOC);
+        $this->assertNotEmpty($data);
+        $this->assertEquals(41, count($data));
+        $this->assertArrayHasKey('id', $data[0]);
+        $this->assertNotEmpty($data[0]['id']);
+        $this->assertArrayHasKey('Value', $data[0]);
+        $this->assertNotEmpty($data[0]['Value']);
+        $this->assertArrayHasKey('RoundPrecision', $data[0]);
+    }
+}
