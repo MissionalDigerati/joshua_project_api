25692d6a325a4f96b5b400e43f3a6c03c138bd8e
Codemis <johnathan@jpulos.com>
2024-11-12 13:47:26 -0800

    Added the People Group Global query generator and the method findById.

---
+++ App/v1/QueryGenerators/PeopleGroupGlobal.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * This file is part of Joshua Project API.
+ *
+ * Joshua Project API is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Joshua Project API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ */
+
+declare(strict_types=1);
+
+namespace QueryGenerators;
+
+use SebastianBergmann\Type\VoidType;
+
+/**
+ * Generates the PDO prepared statements and variables for People Groups globally.
+ *
+ * A class that creates the prepared statement, and sets up the variables for a PDO prepared statement query.
+ * Once you call a method like findById,  you can get the prepared statement by reading the class variable
+ * $preparedStatement.  You can retrieve the prepared variables by reading the class variable $preparedVariables.
+ * So here is an example using the Continents Query Generator to find a continent by id:
+ * <pre><code>
+ * &lt;?php
+ * // Initialize the class, and pass in the id.
+ * $continent = new \QueryGenerators\Continent(array('id' => 'AFR'));
+ * // Call the method you want.
+ * $continent->findById();
+ * // Using PDO prepare the statement.
+ * $statement = $db->prepare($continent->preparedStatement);
+ * // Execute the query with the prepared params.
+ * $statement->execute($continent->preparedVariables);
+ * // Fetch the final results.
+ * $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+ * ?&gt;
+ * </code></pre>
+ *
+ * @author Johnathan Pulos
+ * @package QueryGenerators
+ */
+class PeopleGroupGlobal extends QueryGenerator
+{
+    /**
+     * An array of column names for this database table that we want to select in searches.  Simply remove fields you
+     * do not want to expose.
+     *
+     * @var     array
+     * @access  protected
+     */
+    protected $fieldsToSelectArray = [
+        'PeopleID3', 'PeopName', 'PeopleID1', 'PeopleID2', 'AffinityBloc', 'PeopleCluster', 'ROP3',
+        'ROP25', 'ROP25Name', 'JPScalePGAC', 'PopulationPGAC', 'LeastReachedPGAC', 'FrontierPGAC',
+        'CntPGIC', 'CntUPG', 'CntFPG', 'ROG3Largest', 'CtryLargest', 'ROL3PGAC', 'PrimaryLanguagePGAC',
+        'RLG3PGAC', 'PrimaryReligionPGAC', 'PercentChristianPGAC', 'PercentEvangelicalPGAC'
+    ];
+    /**
+     * The database table to pull the data from.
+     *
+     * @var     string
+     * @access  protected
+     */
+    protected $tableName = "jppeoplesglobal";
+    /**
+     * A string that will hold the default MySQL ORDER BY for the Select statement.
+     *
+     * @var     string
+     * @access  protected
+     */
+    protected $defaultOrderByStatement = "ORDER BY PeopleID3 ASC";
+    /**
+     * An array of table columns (key) and their alias (value).
+     *
+     * @var     array
+     * @access  protected
+     **/
+    protected $aliasFields = [];
+        /**
+     * Construct the People Group Global class.
+     *
+     * During construction,  the $getParams are checked and inserted in the $providedParams class variable.
+     * Some of the methods in this class require certain keys to be set, or it will throw an error.  The comments will
+     * state the required keys.
+     *
+     * @param   array   $getParams  The GET params to use for the query.
+     * @return  void
+     * @access  public
+     * @author  Johnathan Pulos
+     */
+    public function __construct(array $getParams)
+    {
+        parent::__construct($getParams);
+        $this->selectFieldsStatement = join(', ', $this->fieldsToSelectArray);
+    }
+
+    /**
+     * Find a specific people group by the id. (PeopleID3)
+     *
+     * @return  void
+     * @access  public
+     * @throws  \InvalidArgumentException   If the 'id' key is not set on the $providedParams class variable.
+     * @author  Johnathan Pulos
+     */
+    public function findById(): void
+    {
+        $this->validator->providedRequiredParams($this->providedParams, ['id']);
+        $id = intval($this->providedParams['id']);
+        $this->preparedStatement = "SELECT {$this->selectFieldsStatement} FROM {$this->tableName} WHERE PeopleID3 = :id";
+        $this->preparedVariables = ['id' => $id];
+    }
+}
---
+++ Tests/v1/Unit/QueryGenerators/PeopleGroupGlobalTest.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * This file is part of Joshua Project API.
+ *
+ * Joshua Project API is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Joshua Project API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ */
+
+ declare(strict_types=1);
+
+namespace Tests\v1\Unit\QueryGenerators;
+
+use PHPUnit\Framework\TestCase;
+use QueryGenerators\PeopleGroupGlobal;
+
+
+
+/**
+ * Test the Query Generator for the People Group Global Data
+ *
+ * @author Johnathan Pulos
+ */
+class PeopleGroupGlobalTest extends TestCase
+{
+
+    private $db;
+
+    public function setUp(): void
+    {
+        $this->db = getDatabaseInstance();
+    }
+
+    public function testInitializationShouldSanitizeProvidedData(): void
+    {
+        $data = ['country' => 'AZX#%', 'state' => 'AZ%$'];
+        $expected = ['country' => 'AZX', 'state' => 'AZ'];
+        $reflectionOfPeopleGroup = new \ReflectionClass('\QueryGenerators\PeopleGroupGlobal');
+        $providedParams = $reflectionOfPeopleGroup->getProperty('providedParams');
+        $providedParams->setAccessible(true);
+        $result = $providedParams->getValue(new PeopleGroupGlobal($data));
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testFindByIdShouldReturnCorrectPeopleGroup(): void
+    {
+        $peopleGroup = new PeopleGroupGlobal(['id' => 22279]);
+        $peopleGroup->findById();
+        $statement = $this->db->prepare($peopleGroup->preparedStatement);
+        $statement->execute($peopleGroup->preparedVariables);
+        $data = $statement->fetchAll(\PDO::FETCH_ASSOC);
+        $this->assertEquals(1, count($data));
+        $this->assertEquals(22279, $data[0]['PeopleID3']);
+        $this->assertEquals('Pashtun Tirahi', $data[0]['PeopName']);
+        $this->assertEquals(21, $data[0]['PeopleID1']);
+        $this->assertEquals('South Asian Peoples', $data[0]['AffinityBloc']);
+        $this->assertEquals(463, $data[0]['PeopleID2']);
+        $this->assertEquals('South Asia Muslim - Pashtun', $data[0]['PeopleCluster']);
+        $this->assertEquals(119298, $data[0]['ROP3']);
+        $this->assertEquals(306585, $data[0]['ROP25']);
+        $this->assertEquals('Pashtun', $data[0]['ROP25Name']);
+        $this->assertEquals(1, $data[0]['JPScalePGAC']);
+        $this->assertEquals(29000, $data[0]['PopulationPGAC']);
+        $this->assertEquals('Y', $data[0]['LeastReachedPGAC']);
+        $this->assertEquals('Y', $data[0]['FrontierPGAC']);
+        $this->assertEquals(1, $data[0]['CntPGIC']);
+        $this->assertEquals(1, $data[0]['CntUPG']);
+        $this->assertEquals(1, $data[0]['CntFPG']);
+        $this->assertEquals('PK', $data[0]['ROG3Largest']);
+        $this->assertEquals('Pakistan', $data[0]['CtryLargest']);
+        $this->assertEquals('pbu', $data[0]['ROL3PGAC']);
+        $this->assertEquals('Pashto, Northern', $data[0]['PrimaryLanguagePGAC']);
+        $this->assertEquals(6, $data[0]['RLG3PGAC']);
+        $this->assertEquals('Islam', $data[0]['PrimaryReligionPGAC']);
+        $this->assertEquals(0.000, $data[0]['PercentChristianPGAC']);
+        $this->assertEquals(0.000, $data[0]['PercentEvangelicalPGAC']);
+    }
+
+    public function testFindByIdShouldThrowExceptionIfIdIsNotProvided(): void
+    {
+        $this->expectException(\Exception::class);
+        $peopleGroup = new PeopleGroupGlobal([]);
+        $peopleGroup->findById();
+    }
+}
